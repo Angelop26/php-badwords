dcaceecfacb33a8b3a3e95ebb3dcdd1e6cecbbab
Angelop26 <angeloprospero26@gmail.com>
2023-05-03 14:05:56 +0200

    project complete

--- greeting.php
+++ greeting.php
@@ -1,6 +1,7 @@
 <?php
 $text = $_GET['testo_lungo'];
-$cancel = $_GET['error_word'];
+$cancelword = $_GET['error_word'];
+$textcancelled = str_replace($cancelword,'***',$text);
 ?>
 
 <!DOCTYPE html>
@@ -12,8 +13,18 @@ $cancel = $_GET['error_word'];
     <title>Document</title>
 </head>
 <body>
-
+<h2>testo completo</h2>
 <p> <?php echo "$text";?> </p>
+<h2>lunghezza del testo</h2>
+<p> <?php echo strlen($text) . ' caratteri';?> </p>
+<h2>testo con parola censurata</h2>
+<p>
+    <?php echo "$textcancelled"?>
+</p>
+<h2>lunghezza del testo</h2>
+<p>
+    <?php echo strlen($textcancelled) . ' caratteri'; ?>
+</p>
     
 </body>
 </html>
